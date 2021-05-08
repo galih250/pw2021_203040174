ba0ab3e4d25b9902e8b6c040ae899927ab9fd25a
galih250 <galihk250@gmail.com>
2021-05-08 06:38:30 +0300

    penyelesaian tugas

---
+++ pertemuan11/pw_203040174.sql
@@ -0,0 +1,74 @@
+-- phpMyAdmin SQL Dump
+-- version 4.8.5
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: May 08, 2021 at 05:21 AM
+-- Server version: 10.1.38-MariaDB
+-- PHP Version: 7.3.2
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `pw_203040174`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `mahasiswa`
+--
+
+CREATE TABLE `mahasiswa` (
+  `id` int(10) NOT NULL,
+  `gambar` varchar(100) NOT NULL,
+  `nama` varchar(100) NOT NULL,
+  `nrp` varchar(100) NOT NULL,
+  `email` varchar(100) NOT NULL,
+  `jurusan` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `mahasiswa`
+--
+
+INSERT INTO `mahasiswa` (`id`, `gambar`, `nama`, `nrp`, `email`, `jurusan`) VALUES
+(1, 'linggih.jpg', 'Linggih kusumah Dilagga', '203040174', 'galihk250@gmail.com', 'informatika'),
+(2, 'izma.jpeg', 'izma ahmad ', '203040175', 'izmaahmad45@gmail.com', 'komunikasi'),
+(3, 'ahmad.jpeg', 'ahmad ramdhani', '203040176', 'ahmadramdhani90@gmail.com', 'industri'),
+(5, 'salma.jpeg', 'khairunnisa salma', '203040178', 'khairunnisa.salma125@gmail.com', 'perawat'),
+(6, 'aida.jpeg', 'aida listia', '2030401777', 'aidalistia80@gmail.com', 'hukum');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `mahasiswa`
+--
+ALTER TABLE `mahasiswa`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `mahasiswa`
+--
+ALTER TABLE `mahasiswa`
+  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
